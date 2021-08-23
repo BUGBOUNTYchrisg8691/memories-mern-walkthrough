80994e0ae386d2360a3f0593da142d3dfcbcec68
Chris Girvin <bugbounty.chrisg8691@gmail.com>
2021-08-23 04:31:44 -0400

    connection to mongo acquired and posts router initialized

--- server/index.js
+++ server/index.js
@@ -1,4 +1,32 @@
-import express from 'express';
-import bodyParser from 'bodyParser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+// import routers
+const postsRouter = require("./routes/posts");
+
+// app initialization
+const app = express();
+
+// routers
+app.use("/posts", postsRouter);
+
+// app configuration
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// env variables
+require("dotenv").config();
+const DB_CONN_URL = process.env.MONGO_CONN_URL;
+const PORT = process.env.PORT || 5000;
+
+// mongo connection
+mongoose
+  .connect(DB_CONN_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((err) => console.log(err.message));
+mongoose.set("useFindAndModify", false);
