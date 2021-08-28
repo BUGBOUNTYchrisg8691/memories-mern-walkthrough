8dcbcb366699f07c24e2fd52c2a0983005b62627
Chris Girvin <bugbounty.chrisg8691@gmail.com>
2021-08-28 19:04:01 -0400

    fixed getAllPosts bug with client

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,32 +1,32 @@
 // post actions module
 // import api module
-import { api } from '../api';
+import { api } from "../api";
 
 // import action types
-import ATs from './actionTypes';
+import ATs from "./actionTypes";
 
 // action creators
 const getAllPosts = () => async (dispatch) => {
-	try {
-		const { data } = await api.posts.fetchAllPosts();
-		dispatch({ type: ATs.FETCH_ALL_POSTS, payload: data });
-	} catch (err) {
-		console.log('getAllPosts', { err });
-	}
+  try {
+    const { data } = await api.fetchAllPosts();
+    dispatch({ type: ATs.FETCH_ALL_POSTS, payload: data });
+  } catch (err) {
+    console.log("getAllPosts", { err });
+  }
 };
 
 const createPost = (newPost) => async (dispatch) => {
-	try {
-		const { data } = await api.createPost(newPost);
-		dispatch({ type: ATs.CREATE_POST, payload: data });
-	} catch (err) {
-		console.log('createPost', { err });
-	}
+  try {
+    const { data } = await api.createPost(newPost);
+    dispatch({ type: ATs.CREATE_POST, payload: data });
+  } catch (err) {
+    console.log("createPost", { err });
+  }
 };
 
 const postActions = {
-	getAllPosts,
-	createPost,
+  getAllPosts,
+  createPost,
 };
 
 export default postActions;
