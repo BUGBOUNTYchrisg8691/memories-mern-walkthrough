69d3b8f89ebf8a3136578dea848f28107d4c7f55
Chris Girvin <bugbounty.chrisg8691@gmail.com>
2021-08-31 23:06:26 -0400

    mostly done with posts ui; moving on to post functionality

--- client/src/views/posts/Posts.js
+++ client/src/views/posts/Posts.js
@@ -1,28 +1,36 @@
 // posts component
 //import deps
-import React from "react";
-import { useSelector } from "react-redux";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
 
 // import subview components
-import { Post } from "./post";
+import { Post } from './post';
 
 //import styles
-import useStyles from "./styles";
+import useStyles from './styles';
 
 // initialize posts arrow function component
 const Posts = () => {
-  const posts = useSelector((state) => state.posts);
-  const classes = useStyles();
+	const posts = useSelector((state) => state.posts);
+	const classes = useStyles();
+	const { container } = classes;
 
-  console.log({ posts });
-
-  return (
-    <>
-      <h1>Post</h1>
-      <Post />
-      <Post />
-    </>
-  );
+	return (
+		<>
+			{!posts.length ? (
+				<CircularProgress />
+			) : (
+				<Grid className={container} container alignItems="stretch" spacing={3}>
+					{posts.map((post) => (
+						<Grid key={post._id} item xs={12} sm={6}>
+							<Post post={post} />
+						</Grid>
+					))}
+				</Grid>
+			)}
+		</>
+	);
 };
 
 // export posts component
--- client/src/views/posts/post/Post.js
+++ client/src/views/posts/post/Post.js
@@ -1,15 +1,72 @@
 // post component
 // import deps
-import React from "react";
+import React from 'react';
+import {
+	Button,
+	Card,
+	CardActions,
+	CardContent,
+	CardMedia,
+	Typography,
+} from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import moment from 'moment';
 
 // import styles
-import useStyles from "./styles";
+import useStyles from './styles';
 
 // initialize post view arrow function component
-const Post = () => {
-  const classes = useStyles();
+const Post = ({ post }) => {
+	const {
+		createdAt,
+		creator,
+		likeCount,
+		message,
+		selectedFile,
+		tags,
+		title: postTitle,
+	} = post;
+	const classes = useStyles();
+	const { card, cardActions, details, media, overlay, overlay2, title } =
+		classes;
 
-  return <div>Post</div>;
+	return (
+		<Card className={card}>
+			<CardMedia className={media} image={selectedFile} title={postTitle} />
+			<div className={overlay}>
+				<Typography variant="h6">{creator}</Typography>
+				<Typography variant="body2">{moment(createdAt).fromNow()}</Typography>
+			</div>
+			<div className={overlay2}>
+				<Button style={{ color: 'white' }} size="small" onClick={() => {}}>
+					<MoreHorizIcon fontSize="default" />
+				</Button>
+			</div>
+			<div className={details}>
+				<Typography variant="body2" color="textSecondary">
+					{tags.map((tag) => `#${tag} `)}
+				</Typography>
+			</div>
+			<CardContent>
+				<Typography className={title} variant="h5" gutterBottom>
+					{message}
+				</Typography>
+			</CardContent>
+			<CardActions className={cardActions}>
+				<Button size="small" color="primary" onClick={() => {}}>
+					{likeCount}&nbsp;
+					<ThumbUpAltIcon fontSize="small" />
+					Like;
+				</Button>
+				<Button size="small" color="primary" onClick={() => {}}>
+					<DeleteIcon fontSize="small" />
+					Delete
+				</Button>
+			</CardActions>
+		</Card>
+	);
 };
 
 // export post component
